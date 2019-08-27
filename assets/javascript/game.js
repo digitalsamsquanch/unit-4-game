0032ab9f435a62c70cc2d4e2850368be7d2a6cb7
Digital Samsquanch <digital.samsquanch@protonmail.com>
2019-08-27 10:34:53 -0400

    write out sudo code overview for my work

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+/* 
+- make variable for each playable character
+- for each character make a variable for their hitpoints
+- create a counterattack power level for each character and assign variable
+- when character is selected, move the rest of the characters to the "enemies available to attack" area
+- when user selects enemy, move that enemy to the "Defender" area
+- create attack button
+- set base attack power at 6 and set it to increase by 6 every time attack takes place
+- subtract the attacks from the hitpoints of the defender
+- have a counter attack by the defender after each attack from the player and subtract that from the player hitpoints
+- end the game if player reaches 0 hitpoints
+- have user select new defender when they defeat a defender
+- once final defender is beaten declare the player the winner
+*/
