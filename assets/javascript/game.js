57a95af21cec59189ba2472cb497a52985631dba
digitalsamsquanch <digital.samsquanch@protonmail.com>
2019-09-03 09:43:23 -0400

    working on creating the cards with jQuery as well as the card movement

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,53 @@
+var chosen;
+var myChar = "";
+
+var characters = {
+    rey: {
+        name:  "Rey",
+        hitPoints: 150,
+        counterattack: 20
+    },
+    boba: {
+        name: "Boba Fett",
+        hitPoints: 100,
+        counterattack: 50
+    },
+    grievous: {
+        name: "Grievous",
+        hitPoints: 200,
+        counterattack: 10
+    },
+    phasma: {
+        name: "Phasma",
+        hitPoints: 180,
+        counterattack: 15
+    }
+};
+
 $(document).ready(function(){
 /* 
 - make object for each playable character
 - for each character make a variable for their hitpoints
 */
-var reyCard = [
-    {
-        name:  "Rey",
-        hitPoints: 150,
-        counterattack: 20
-    },
-    {
-        name: "Boba Fett",
-        hitPoints: 100,
-        counterattack: 50
-    },
-    {
-        name: "Grievous",
-        hitPoints: 200,
-        counterattack: 10
-    },
-    {
-        name: "Phasma",
-        hitPoints: 180,
-        counterattack: 15
+
+$(".charCol").click(function(){
+    if(myChar == "") {
+        myChar = $(this);
+        console.log(myChar);
+        chosen = $(myChar).attr("value");
+        console.log(chosen)
+    };
+    // console.log(chosen)
+    if(chosen == "rey"){
+        
     }
-]
+});
+
+    
 /*
 - create a counterattack power level for each character and assign variable
 - when character is selected, move the rest of the characters to the "enemies available to attack" area
+
 - when user selects enemy, move that enemy to the "Defender" area
 - create attack button
 - set base attack power at 6 and set it to increase by 6 every time attack takes place
